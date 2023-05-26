8356a78434ccaabd0806dbeacca90411d54ce240
Dreamer-byte <parcestar@gmail.com>
2023-05-26 14:43:11 -0600

    Implementacion de los metodos de la clase operaciones

--- Calculadora/Operaciones.cs
+++ Calculadora/Operaciones.cs
@@ -3,21 +3,34 @@ public class Operaciones
     #region Noel
     public int Suma(int a, int b)
     {
-        throw new NotImplementedException("No Implementado");
+        // throw new NotImplementedException("No Implementado");
+
+        return a + b;
     }
     #endregion
     public int Resta(int a, int b)
     {
-        throw new NotImplementedException("No Implementado");
+        // throw new NotImplementedException("No Implementado");
+        return a -b;
     }
 
     public int Multiplicacion(int a, int b)
     {
-        throw new NotImplementedException("No Implementado");
+        // throw new NotImplementedException("No Implementado");
+
+        return a * b;
     }
 
     public int Division(int a, int b)
     {
-        throw new NotImplementedException("No Implementado");
+        // if (a == 0 || b == 0){
+        //     throw new NotImplementedException("No existe division por cero");
+        // }
+
+        if (a == 0 || b == 0){
+            return 0;
+        }
+
+        return a /b;    
     }
 }
